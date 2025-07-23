1b53816ee8341fb9c94dbb71749fca93e0f6ad82
jagan2292 <jagan2292@gmail.com>
2025-07-23 10:56:46 +0530

    Added first table script for Country Codes

---
+++ Create_Table.sql
@@ -0,0 +1,17 @@
+
+/****** Object:  Table [dbo].[Country_DialCodes]    Script Date: 7/23/2025 10:52:08 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Country_DialCodes](
+	[ISO Code 3] [nvarchar](255) NULL,
+	[ISO Code 2] [nvarchar](255) NULL,
+	[Country Code] [nvarchar](255) NULL,
+	[Country] [nvarchar](255) NULL
+) 
+GO
+
+
