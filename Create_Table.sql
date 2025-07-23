36471e237bbe2ab89ad55276208983f7abc85ba9
jagan-mani <jagan.mani@kemin.com>
2025-07-23 11:38:51 +0530

    Added comments and improved formatting

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,4 +1,10 @@
 
+/***************************************
+Purpose: Create CountryCode table to store country codes and related information
+Author: jaganmani
+Date Created: 2025-07-23
+***************************************/
+
 /****** Object:  Table [dbo].[Country_DialCodes]    Script Date: 7/23/2025 10:52:08 AM ******/
 SET ANSI_NULLS ON
 GO
@@ -6,12 +12,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Country_DialCodes](
-	[ISO Code 3] [nvarchar](255) NULL,
-	[ISO Code 2] [nvarchar](255) NULL,
-	[Country Code] [nvarchar](255) NULL,
-	[Country] [nvarchar](255) NULL
-) 
+CREATE TABLE [dbo].[CountryCode] (
+	[ISO Code 3]   NVARCHAR(255) NULL,
+	[ISO Code 2]   NVARCHAR(255) NULL,
+	[Country Code] NVARCHAR(255) NULL,
+	[Country]      NVARCHAR(255) NULL
+);
 GO
 
 
